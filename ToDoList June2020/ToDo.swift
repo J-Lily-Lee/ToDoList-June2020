0c8a449a223c62f483fac8d817d27d6ad3a99643
J-Lily-Lee <violinpercussion@gmail.com>
2020-06-23 13:36:26 -0500

    Initial Commit

---
+++ ToDoList June2020/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  ToDoList June2020
+//
+//  Created by Lily Lee on 6/22/20.
+//  Copyright © 2020 Lily Lee. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
+
---
+++ ToDoList June2020/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList June2020
+//
+//  Created by Lily Lee on 6/22/20.
+//  Copyright © 2020 Lily Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
